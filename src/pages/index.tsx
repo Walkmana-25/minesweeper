c3ef7931a1b188d2baebd5891f0afdcc983ebcdf
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-18 23:48:31 +0900

    init board

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,34 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
+const generate2DArray = (m: number, n: number, val = 0) => {
+  return [...Array(m)].map(() => Array(n).fill(val));
+};
+
 const Home = () => {
+  //REMOVE THIS
+  const vertical = 5;
+  const horizontal = 5;
+
+  const [bombPos, setBombPos] = useState<number[][]>(generate2DArray(vertical, horizontal, 0));
+
   const [samplePos, setSamplePos] = useState(0);
   console.log(samplePos);
   return (
-    <div className={styles.container}>
-      <div className={styles.sample} style={{ backgroundPosition: `-${30 * samplePos}px 0px` }} />
-      <button onClick={() => setSamplePos((p) => (p + 1) % 14)}>sample</button>
-      <p>{samplePos + 1}</p>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.sample} style={{ backgroundPosition: `-${30 * samplePos}px 0px` }} />
+        <button onClick={() => setSamplePos((p) => (p + 1) % 14)}>sample</button>
+        <p>{samplePos + 1}</p>
+      </div>
+      <div>
+        {bombPos.map((row) =>
+          row.map(() => {
+            console.log('hi');
+            return null; // Replace void[] with null
+          }),
+        )}
+      </div>
     </div>
   );
 };
